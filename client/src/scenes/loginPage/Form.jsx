546b1f4b8cc47ca11732fa097c160dcaffbd4c37
Russell Obatay <98654047+russxl@users.noreply.github.com>
2023-11-14 15:08:21 +0800

    Update Form.jsx

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -64,7 +64,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "https://nu-social-app.onrender.com/auth/register",
+      "https://nu-backend-anz5.onrender.com/auth/register",
       {
         method: "POST",
         body: formData,
@@ -79,7 +79,7 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("https://nu-social-app.onrender.com/auth/login", {
+    const loggedInResponse = await fetch("https://nu-backend-anz5.onrender.com/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
